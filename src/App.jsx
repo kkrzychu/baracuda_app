6aaec664a85d5b9d4c5e57d3248c9b4c062d4416
Krzysztof Michalewicz <krzymich1997@gmail.com>
2024-03-10 17:07:18 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import Navbar from './components/Navbar'
+
+const App = () => (
+
+    <div className='bg-primary w-full overflow-hidden'>
+      <h1>Hello World</h1>
+
+      <Navbar>
+
+      </Navbar>
+
+    </div>
+
+)
+
+
+export default App
